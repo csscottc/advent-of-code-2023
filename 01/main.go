c626abaf03bbedd34657d4978e5491e6ad171943
Scott Carpenter <scottcarpenter01@gmail.com>
2023-12-08 20:18:41 +0000

    Finally finished.

--- 01/main.go
+++ 01/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"strconv"
 	"unicode"
+  "strings"
 )
 
 func main() {
@@ -27,18 +28,29 @@ func main() {
 			}
 			break
 		}
-		calibrationValue := getCalibrationValue(line)
-		num, err := strconv.Atoi(calibrationValue)
-
-		if err != nil {
-			fmt.Println(err)
-		}
-		calibrationValues = append(calibrationValues, num)
+		calibrationValue := GetCalibrationValue(line)
+		calibrationValues = append(calibrationValues, calibrationValue)
 	}
-	fmt.Println(sumCalibrationValues(calibrationValues))
+  fmt.Println("The sum is:",SumCalibrationValues(calibrationValues))
 }
 
-func sumCalibrationValues(cvalues []int) int {
+func lookup(word string) (rune, bool) {
+  m := map[string]rune{
+    "one": '1',
+    "two": '2',
+    "three": '3',
+    "four": '4',
+    "five": '5',
+    "six": '6',
+    "seven": '7',
+    "eight": '8',
+    "nine": '9',
+  }
+  val, ok := m[word]
+  return val, ok
+}
+
+func SumCalibrationValues(cvalues []int) int {
 	var total = 0
 	for _, val := range cvalues {
 		total += val
@@ -46,18 +58,42 @@ func sumCalibrationValues(cvalues []int) int {
 	return total
 }
 
-func getCalibrationValue(line string) string {
+func GetCalibrationValue(line string) int {
 	var first rune
 	var last rune
-	var firstAssigned = false
+	var isFirstAssigned = false
+  var buff []rune
+  var numberWords = []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+
 	for _, rv := range line {
-		if unicode.IsDigit(rune(rv)) {
-			if firstAssigned == false {
+    if unicode.IsDigit(rune(rv)) {
+			if isFirstAssigned == false {
 				first = rv
-				firstAssigned = true
+				isFirstAssigned = true
 			}
 			last = rv
-		}
+    } else {
+      buff = append(buff, rv)
+      for _, word := range numberWords {
+        if strings.Contains(string(buff), word) {
+          v, _ := lookup(word)
+          if isFirstAssigned == false {
+            first = v
+            isFirstAssigned = true
+          }
+          last = v
+          // There's a case where eighthree should be parsed as 83.
+          // Clear down all but the last char in the buffer to workaround this.
+          // Its icky but works.
+          buff = buff[len(string(buff))-1:]
+        } 
+      }
+    }
 	}
-	return fmt.Sprintf("%c%c", first, last)
+
+	num, err := strconv.Atoi(fmt.Sprintf("%c%c", first, last))
+  if err != nil {
+    fmt.Println(err)
+  }
+  return num
 }
